c81d06cd9ef173121d3aac649721c455bdab3683
ArmndoHg <armndohernndez@gmail.com>
2022-10-29 00:10:49 +0200

    Increase and decrease button

--- D5.js
+++ D5.js
@@ -1,4 +1,5 @@
 //----------ADDING NAMES TO WAITING LIST------------ //
+
 const input = document.getElementById('newName');
 const waitingList  = document.getElementById('waitingList')
 const addbtn = document.getElementById('addbtn');
@@ -7,6 +8,44 @@ const addingName = function(){
     const list = document.createElement('p');
     list.innerText = input.value;
     waitingList.appendChild(list);
-    }
+    clear();
+}
 
-    addbtn.addEventListener('click',addingName)
+const clear = function(){
+    input.value = "";
+}
+
+addbtn.addEventListener('click', addingName, clear)
+
+
+    //---------SELECTING TEAMS----------//
+
+let minusBtn = document.querySelector('.minus')
+let plusBtn = document.querySelector('.plus')
+let teamInput = document.querySelector('.amountTeams')
+
+let count = 0;
+teamInput.value = count;
+teamInput.innerText = teamInput.value;
+
+//------------INCREASE TEAMS------------//
+
+const plusingTeams = function() {
+    count++;
+    teamInput.value = count;
+    teamInput.innerText = teamInput.value;
+    return;
+}
+
+plusBtn.addEventListener("click", plusingTeams);
+
+//------------DECREASE TEAMS------------//
+
+const restingTeams = function(){
+    count--;
+    teamInput.value = count;
+    teamInput.innerText = teamInput.value;
+    return;
+}
+
+minusBtn.addEventListener('click',restingTeams);
